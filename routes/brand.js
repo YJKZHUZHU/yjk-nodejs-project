22922e03e7f48feaf3fc21c71f0f17165ce43420
wawawa <2730455209@qq.com>
2019-04-05 11:38:48 +0800

    'ppp'

--- routes/brand.js
+++ routes/brand.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var brandModel = require('../model/brandModels')
 const multer = require('multer')
+
 const upload = multer({
 	dest: 'G:/img/'
 })
